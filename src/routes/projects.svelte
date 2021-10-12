812f187cc9354437dc6a8881094d002c90a6318b
edith <edith.maulandi@makina-corpus.com>
2021-10-12 19:53:01 +0200

    Display test data from a csv

---
+++ src/routes/projects.svelte
@@ -0,0 +1,19 @@
+<script>
+  import projects from "$data/projects.csv";
+  console.log(projects);
+</script>
+
+<svelte:head>
+	<title>Projects</title>
+</svelte:head>
+
+<div class="content">
+  <h1>Hello</h1>
+  {#each projects as { title, type, description }}
+    <div>
+      <h2>{title}</h2>
+      <p>{type}</p>
+      <p>{description}</p>
+    </div>
+  {/each}
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,22 @@
 /** @type {import('@sveltejs/kit').Config} */
+import path from "path";
+import dsv from "@rollup/plugin-dsv";
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			resolve: {
+				alias: {
+					"$data": path.resolve("./src/data"),
+				}
+			},
+			plugins: [
+				dsv()
+			]
+		},
+		ssr: false,
 	}
 };
 
