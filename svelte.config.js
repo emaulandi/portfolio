fe72ed10b118521393359e945ba546e0364b7c8a
edith <edith.maulandi@makina-corpus.com>
2021-10-15 13:48:13 +0200

    Update config and path for github pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,20 @@
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 import path from "path";
 import dsv from "@rollup/plugin-dsv";
+import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		vite: {
 			resolve: {
 				alias: {
@@ -17,6 +26,10 @@ const config = {
 			]
 		},
 		ssr: false,
+		paths: {
+			base: dev ? '' : '/portfolio',
+		},
+		trailingSlash: "always",
 	}
 };
 
