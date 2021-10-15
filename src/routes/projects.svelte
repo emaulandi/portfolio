ab5970e4abaf5659e0c18e2221bfde3902edefdd
edith <edith.maulandi@makina-corpus.com>
2021-10-15 15:32:13 +0200

    Git add real projects data

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,5 +1,6 @@
 <script>
   import projects from "$data/projects.csv";
+  import { base } from '$app/paths';
   console.log(projects);
 </script>
 
@@ -9,11 +10,13 @@
 
 <div class="content">
   <h1>Hello</h1>
-  {#each projects as { title, type, description }}
+  {#each projects as { title, type, img, tech, link, description }}
     <div>
       <h2>{title}</h2>
       <p>{type}</p>
-      <p>{description}</p>
+      <p>{@html description}</p>
+      <p>Tech {tech.split(',')}</p>
+      <img width='300px' src={`${base}/images/${img}`} alt={`${base}/images/${img}`}>
     </div>
   {/each}
 </div>
